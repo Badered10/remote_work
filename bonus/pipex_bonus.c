9176315166fab60a1f223147e61a209d64f8cd77
baouragh <baouragh@e2r9p12.1337.ma>
2024-02-12 16:19:36 +0100

    add pseudocode to pipex.c and pipex_bonus.c

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:45:53 by baouragh          #+#    #+#             */
-/*   Updated: 2024/02/12 10:18:01 by baouragh         ###   ########.fr       */
+/*   Updated: 2024/02/12 16:18:28 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,3 +76,56 @@ int	main(int argc, char **argv, char **env)
 		return (check_last_cmd(argv[argc - 2]));
 	return (check_out_fd(fd.check_out, argc, argv));
 }
+
+/*
+	for this project the the main key of succeed is execve()
+	it needs 3 arguments , fullpath of executable file (cmd)
+	and the cmd plus its args(double char pointer), and the
+	last argument of this func is environment pointer .
+	if execve() obtained all its args succefully it will 
+	be the end of the current process that call execve() and it will
+	lunch new process that replace the old one with the cmd
+	we asking for.
+	
+
+	almost same algo as madantory part despite checking in args 
+	msut be at least 5 and (here_doc case and multiple pipes)...
+	
+	pseudocode:
+	
+	1 ---> check args must be at least 5; otherways threw error.
+	
+	2 ---> if the arg 2 not "here_doc"open fd for infile 
+	and outfile ; if fd is right dup it to stdin to pass its content
+	as input to the pipe; otherways if fd false open a tmp file to
+	pass its empty content to hundle such cases like command cat taht
+	need input or will hang.
+	in here_doc case open fd for a hidden file to write in it 
+	the input that user write in stdin, and open outfile file
+	
+	2.1 ---> re open the hidden file to get a new file pointer 
+	the offset of it will be 0 to past it as input to the first cmd 
+	
+	3 ---> check cmds if they are right depending of mods cases,
+	print in stderr error if command not found and continue the process
+	
+	4 ---> create pipe to contact with processes
+	
+	5 ---> fork the code to make copy of its ressources.
+	
+	6 --->  in child close inused fds and write output to pipe
+	
+	7 ---> in parent close inused write to pipe fd , 
+	and dup stdin to pipe , and unlink the hidden file if exist.
+	
+	8 ---> repeat from 4 to 7 (cmds -1) times 
+	
+	8.1 ----> in last cmd case in 6 write to outfile file instead of pipe.
+	
+	9 ---> in parent wait for all childs processes to finsh
+	
+	10 ---> if outfile created succeesfully check last cmd 
+	if its true ot false , if its true do nothing , else threw the 
+	the right return value (126 permission denied)(127 cmd not found)
+	or 0 (succeed)
+*/
--- mandatory_srcs/pipex.c
+++ mandatory_srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: baouragh <baouragh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:45:53 by baouragh          #+#    #+#             */
-/*   Updated: 2024/02/12 10:17:05 by baouragh         ###   ########.fr       */
+/*   Updated: 2024/02/12 16:16:42 by baouragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,14 +51,12 @@ static int	check_last_cmd(char *cmd_set)
 
 int	main(int argc, char **argv, char **env)
 {
-	int		cmds;
 	t_fd	fd;
 	int		i;
 
 	i = 0;
 	if (argc != 5)
 		return (print_err("Invaild number of args .", NULL), INV_ARGS);
-	cmds = argc - (4);
 	fd = open_fds(argc, argv, env, MADANTORY);
 	fd.i_place = i;
 	cmds_checker(fd, argc, argv, env);
@@ -71,3 +69,44 @@ int	main(int argc, char **argv, char **env)
 		return (check_last_cmd(argv[argc - 2]));
 	return (check_out_fd(fd.check_out, argc, argv));
 }
+
+/*
+	for this project the the main key of succeed is execve()
+	it needs 3 arguments , fullpath of executable file (cmd)
+	and the cmd plus its args(double char pointer), and the
+	last argument of this func is environment pointer .
+	if execve() obtained all its args succefully it will 
+	be the end of the current process that call execve() and it will
+	lunch new process that replace the old one with the cmd
+	we asking for.
+	
+	pseudocode;
+	
+	1 ---> check args must be  5; otherways threw error.
+	
+	2 ---> open fd for infile and outfile ; if fd is right 
+	dup it to stdin to pass its content as input to the pipe;
+	otherways if fd false open a tmp file to pass its empty content 
+	to hundle such cases like command cat taht need input or will hang.
+	
+	3 ---> check cmds if they are right depending of mods cases,
+	print in stderr error if command not found and continue the process
+	
+	4 ---> create pipe to contact with processes
+	
+	5 ---> fork the code to make copy of its ressources.
+	
+	6 --->  in child close inused fds and write output to pipe
+	
+	7 ---> in parent close inused write to pipe fd , 
+	and dup stdin to pipe , and unlink the hidden file if exist.
+	
+	8 ---> repeat from 4 to 7 but in 6 write to outfile file instead of pipe.
+	
+	9 ---> in parent wait for all childs processes to finsh
+	
+	10 ---> if outfile created succeesfully check last cmd 
+	if its true ot false , if its true do nothing , else threw the 
+	the right return value (126 permission denied)(127 cmd not found)
+	or 0 (succeed)
+*/
